4159841299c39005b0371882464aa327b31e1ce0
Charizard17 <3723esad@gmail.com>
2023-06-08 22:14:35 +0200

    simple interaction with ui references

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceTwo")
+        diceImageView2.image = UIImage(named: "DiceFour")
     }
 
 
